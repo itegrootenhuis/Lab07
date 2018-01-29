4b0079744773cf08dca967b1d6492b5cf8685b85
itegrootenhuis <itegrootenhuis@bizstream.com>
2018-01-29 16:56:38 -0500

    Created student object and put all the data into json

--- Lab07/Program.cs
+++ Lab07/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +10,59 @@ namespace Lab07
 {
     class Program
     {
+        public static string studentData;
+
         static void Main(string[] args)
         {
+            SetUpStudentData("Ian", "Allendale", "Alfrado");//tegrootenhuis
+            SetUpStudentData("Kevin", "Chicago", "Oatmeal");//davis
+            SetUpStudentData("Adam", "Grand Rapids", "putine");//tasma
+            SetUpStudentData("Aaron", "Hart", "Ribeye");//sandusky
+            SetUpStudentData("Marshal", "Holland", "Burgers");//hatley
+            SetUpStudentData("David", "Fort Wayne", "Lasagna");//wilkins
+            SetUpStudentData("Andrew", "Fremont", "steak ");//patenge
+            SetUpStudentData("Chamus", "Zeeland", "Sushi");//gilbert
+            SetUpStudentData("Jonathan", "Middleville", "Stouts");//higgins
+            SetUpStudentData("Albert", "Grand Rapids", "pounded potatoes with beans");//ngoudjou
+            SetUpStudentData("Tommy", "Raleigh", "Chicken Curry");//waalkes
+
+            Console.WriteLine("Welcome to C# Class. Which student would you like to learn more about? ");
             GetUserInput();
+        }
+
+        private static void SetUpStudentData(string Name, string Hometown, string FavoriteFood)
+        {
+            Student student = new Student();
+            student.Name = Name;
+            student.Hometown = Hometown;
+            student.FavoriteFood = FavoriteFood;
+            studentData += JsonConvert.SerializeObject(student);
         }
 
         private static void GetUserInput()
         {
-            Console.WriteLine("Enter in text you want translated to Pig Latin:");
-            string userInput = Console.ReadLine();
+            Console.WriteLine("Enter in a student's first name: ");
+            //string studentName = Console.ReadLine();
+            dynamic data = JObject.Parse(studentData);
+            Console.WriteLine(data);
+            Console.WriteLine(studentData);
+            QuitConsoleApp();
+            //if (students.Contains(studentName.ToLower()))
+            //{
+            //    SetupNewStudent(studentName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The student you are searching for doesn't exist.");
+            //    QuitConsoleApp();
+            //}
         }
 
+        private static void SetupNewStudent(string studentName)
+        {
+            Student student = new Student();
+            Console.WriteLine("Name: {0}, Favoirte Food: {1}, Hometown: {2}", studentName, student.FavoriteFood, student.Hometown);
+        }
 
         private static void QuitConsoleApp()
         {
---
+++ Lab07/StudentInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string FavoriteFood { get; set; }
+        public string Hometown { get; set; }
+    }
+}
